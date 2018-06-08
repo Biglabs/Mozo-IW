eda90623109de93c96cebb0c74bc93e59f9db93c
vuquangnguyen-biglabs <vu.nguyen.quang@big-labs.com>
2018-06-08 12:59:48 +0700

    create seed from BIP39 recovery phrase

--- SOLOSigner/src/App.js
+++ SOLOSigner/src/App.js
@@ -1,53 +1,66 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * SOLo Signer App
+ * https://github.com/Biglabs/Mozo-IW/tree/master/SOLOSigner
  * @flow
  */
 
 import React, {Component} from 'react';
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    Button
-} from 'react-native';
+import {Button, Picker, Platform, StyleSheet, Text, View} from 'react-native';
 import bip39 from 'bip39';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
 
     constructor(props) {
         super(props);
-        this.state = {mnemonic: instructions};
+        this.state = {
+            language: bip39.wordlists.english,
+            mnemonic: '',
+            seed: ''
+        };
+    }
+
+    generatePhrase() {
+        this.setState({
+            mnemonic: bip39.generateMnemonic(128, null, this.state.language),
+            seed: bip39.mnemonicToSeedHex(this.state.mnemonic)
+        });
     }
 
     render() {
+        const languages = {
+            "English": bip39.wordlists.english,
+            "Japanese": bip39.wordlists.japanese,
+            "Spanish": bip39.wordlists.spanish,
+            "Chinese Simplified": bip39.wordlists.chinese_simplified,
+            "Chinese Traditional": bip39.wordlists.chinese_traditional,
+            "French": bip39.wordlists.french,
+            "Italian": bip39.wordlists.italian,
+            "Korean": bip39.wordlists.korean
+        };
+
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
+                <Picker style={{height: 50, width: 200}}
+                        selectedValue={this.state.language}
+                        onValueChange={(itemValue, itemIndex) => {
+                            this.setState({language: itemValue}, () => this.generatePhrase());
+                        }}>
+                    {Object.keys(languages).map((key) => {
+                        return (<Picker.Item label={key} value={languages[key]}/>)
+                    })}
+                </Picker>
+
+                <Text style={styles.text}>
                     {this.state.mnemonic}
-                    {}
+                    {console.log(this.state.mnemonic)}
+                </Text>
+                <Text style={styles.text}>
+                    {this.state.seed}
+                    {console.log(this.state.seed)}
                 </Text>
 
-                <Button title='test bip39' onPress={() => {
-                    this.setState({
-                        mnemonic: bip39.generateMnemonic()
-                    });
-                }}/>
+                <Button title='create recovery phrase' onPress={() => this.generatePhrase()}/>
             </View>
         );
     }
@@ -60,14 +73,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
+    text: {
         textAlign: 'center',
         color: '#333333',
-        marginBottom: 5,
+        margin: 5,
     },
 });
