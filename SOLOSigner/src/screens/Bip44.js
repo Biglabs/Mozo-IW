f66aa9ee59467bfd6ba22bd0f9c90ed1168e0920
Hoang Nguyen <hoang.nn@big-labs.com>
2018-06-12 16:34:54 +0700

    Implement sign transaction.

--- SOLOSigner/src/screens/Bip44.js
+++ SOLOSigner/src/screens/Bip44.js
@@ -1,28 +1,58 @@
-import React, {Component} from 'react';
-import {Button, Platform, StyleSheet, Text, View, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { Button, Platform, StyleSheet, Text, View, Alert } from 'react-native';
 import Bitcoin from 'react-native-bitcoinjs-lib';
 import bip39 from 'bip39';
+const testnet = 'https://ropsten.infura.io/'; //Onb2hCxHKDYIL0LNn8Ir
+import Web3 from 'web3';
 
 type Props = {};
 export default class Bip44 extends Component<Props> {
-
     constructor(props) {
         super(props);
         this.state = {
             rootKey: '',
             adrBip44Test: '',
+            privKeyBuffer: '',
+            adrTest2: '0x213DE50319F5954D821F704d46e4fd50Fb09B459',
             seed: this.props.seed
         };
     }
 
+    displayAlert(type, content){
+        Alert.alert(
+            type,
+            content,
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: false }
+          )
+    }
 
+    loadBalances() {
+        
+        const walletAddress = this.state.adrBip44Test;
+        const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+        console.log(walletAddress);
+        web3.eth.getBalance(walletAddress).then((data) => {
+            console.log(data);
+            let value = web3.utils.fromWei(data);
+            this.setState({
+                balance: value
+            });
+        });
+        web3.eth.getBalance(this.state.adrTest2).then((data) => {
+            console.log(data);
+            let value = web3.utils.fromWei(data);
+            this.setState({
+                balance2: value
+            });
+        });
+    }
 
     render() {
         return (
             <View key="bip44" style={styles.container}>
-                <Text>
-                    Bip44
-                </Text>
                 <Button title='Generate BTC Wallet' onPress={() => {
                     let seed = this.state.seed;
                     // Generate BIP32 Root Key from BIP39 Seed
@@ -35,7 +65,7 @@ export default class Bip44 extends Component<Props> {
                     this.setState({
                         adrBip44Test: childKey.getAddress()
                     });
-                }}/>
+                }} />
                 <Button title='Generate ETH Wallet' onPress={() => {
                     /*var hdkey = require('ethereumjs-wallet-react-native/hdkey');
                     let seed = this.state.seed;
@@ -60,42 +90,66 @@ export default class Bip44 extends Component<Props> {
                     var bip32ExtendedKey = rootKey.derivePath("m/44'/60'/0'/0");
                     this.setState({
                         rootKey: "Bip32 Private extended key: " + bip32ExtendedKey.toBase58(),
-                        xpub : "Bip32 Public extended key: " + bip32ExtendedKey.neutered().toBase58()
+                        xpub: "Bip32 Public extended key: " + bip32ExtendedKey.neutered().toBase58()
                     });
                     bip32ExtendedKey = bip32ExtendedKey.derive(0);
                     var keyPair = bip32ExtendedKey.keyPair;
-                    var privKeyBuffer = keyPair.d.toBuffer(32);
-                    var privkey = privKeyBuffer.toString('hex');
+                    this.state.privKeyBuffer = keyPair.d.toBuffer(32);
+                    var privkey = this.state.privKeyBuffer.toString('hex');
                     var ethUtil = require('ethereumjs-util');
-                    var addressBuffer = ethUtil.privateToAddress(privKeyBuffer);
+                    var addressBuffer = ethUtil.privateToAddress(this.state.privKeyBuffer);
                     var hexAddress = addressBuffer.toString('hex');
                     var checksumAddress = ethUtil.toChecksumAddress(hexAddress);
                     var address = ethUtil.addHexPrefix(checksumAddress);
-                    console.log("Ethereum address: [" + address  + "]")
+                    console.log("Ethereum address: [" + address + "]")
                     privkey = ethUtil.addHexPrefix(privkey);
                     var pubkey = ethUtil.addHexPrefix(pubkey);
                     this.setState({
                         adrBip44Test: address,
-                        pubkey: "Public key: " + pubkey,
-                        privkey: "Private key: " + privkey
+                        privkey: privkey
                     });
-                }}/>
+                }} />
                 <Button title='View balance' onPress={() => {
-                    let Web3 = require('web3');
-                    const testnet = 'https://ropsten.infura.io/Onb2hCxHKDYIL0LNn8Ir';
-                    const walletAddress = this.state.adrBip44Test;
-                    const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
-                    console.log(walletAddress);
-                    web3.eth.getBalance(walletAddress).then( (data)=> {
-                        console.log(data);
-                        let value = web3.utils.fromWei(data);
-                        this.setState({
-                            balance: "Balance: " + value
-                        });
-                    });
-                    //walletAddress); //Will give value in.
-
-                }}/>
+                    this.loadBalances();
+                }} />
+                <Button title='Send 1 ETH to account 2' onPress={() => {
+                    const EthereumTx = require('ethereumjs-tx');
+                    //Create a new transaction
+                    const txParams = {
+                        nonce: '00',
+                        gasPrice: 5000000000,
+                        gasLimit: 210000,
+                        to: this.state.adrTest2,
+                        value: '1',
+                        data: '0x00',
+                        // EIP 155 chainId - mainnet: 1, ropsten: 3
+                        chainId: 3
+                    }
+                    const tx = new EthereumTx(txParams);
+                    //var key = new Buffer(this.state.privkey, 'hex');
+                    tx.sign(this.state.privKeyBuffer);
+                    const serializedTx = tx.serialize();
+                    const web3 = new Web3(
+                        new Web3.providers.HttpProvider(testnet)
+                    );
+                    //Verify connection is successful
+                    web3.eth.net.isListening()
+                        .then(() => console.log('is connected'))
+                        .catch(e => console.log('Wow. Something went wrong'));
+                    let data = `0x${serializedTx.toString('hex')}`;
+                    web3.eth.sendSignedTransaction(data,
+                        (error, result) => {
+                            if (error) { 
+                                console.log(`Error: ${error}`);
+                                this.displayAlert("Error", error.message); 
+                            }
+                            else { 
+                                console.log(`Result: ${result}`);
+                                this.displayAlert("Result", result.data);
+                            }
+                        }
+                    );
+                }} />
                 <Text style={styles.instructions}>
                     {this.state.seedTest}
                     {}
@@ -109,15 +163,17 @@ export default class Bip44 extends Component<Props> {
                     {}
                 </Text>
                 <Text style={styles.instructions}>
-                    {this.state.adrBip44Test}
+                    Address 1: {this.state.adrBip44Test}
                     {'\n'}
-                    {this.state.pubkey}
+                    Address 1 - Private key: {this.state.privkey}
                     {'\n'}
-                    {this.state.privkey}
+                    Address 2: {this.state.adrTest2}
                 </Text>
                 <Text style={styles.instructions}>
-                    {this.state.balance}
-                    {}
+                    Balance of address 1: {this.state.balance}
+                    {'\n'}
+                    Balance of address 2: {this.state.balance2}
+                    {'\n'}
                 </Text>
             </View>
         );
