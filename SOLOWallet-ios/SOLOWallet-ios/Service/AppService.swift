194c86f0c58618005e60c7c583d7df98d48e7352
Hoang Nguyen <hoang.nn@big-labs.com>
2018-06-27 09:34:21 +0700

    [iOS] Open Safari to view transaction.

--- SOLOWallet-ios/SOLOWallet-ios/Service/AppService.swift
+++ SOLOWallet-ios/SOLOWallet-ios/Service/AppService.swift
@@ -63,6 +63,9 @@ public class AppService {
             let json = SwiftyJSON.JSON(value)
             if let result = json["result"].string {
                 print("TxId: \(result)")
+                //Open browser to view transaction info
+                let url = URL(string: "http://ropsten.etherscan.io/tx/\(result)")
+                UIApplication.shared.open(url!, options: [:], completionHandler: nil)
             }
             
         }
