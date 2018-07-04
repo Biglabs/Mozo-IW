2b08dfa6e10d7ebe29092b0073294c3622251755
Tam Nguyen <tam.nguyen@big-labs.com>
2018-07-04 13:55:10 +0700

    [iOS wallet] update validate send function

---
+++ SOLOWallet-ios/SOLOWallet-ios/UI/Send/BTC+SendViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BTC+SendViewController.swift
+//  SOLOWallet-ios
+//
+//  Created by Tam Nguyen on 7/4/18.
+//  Copyright © 2018 biglabs. All rights reserved.
+//
+
+import Foundation
+
+extension SendViewController {
+    func validateBTC() -> Bool {
+        return false
+    }
+}
---
+++ SOLOWallet-ios/SOLOWallet-ios/UI/Send/ETH+SendViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ETH+SendViewController.swift
+//  SOLOWallet-ios
+//
+//  Created by Tam Nguyen on 7/4/18.
+//  Copyright © 2018 biglabs. All rights reserved.
+//
+
+import Foundation
+
+extension SendViewController {
+    func validateETH() -> Bool {
+        return false
+    }
+}
