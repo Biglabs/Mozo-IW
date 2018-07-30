061fd11bf74e2f37f7c5b1603f9db58d9e4d328a
Hoang Nguyen <hoang.nn@big-labs.com>
2018-07-30 18:00:35 +0700

    [iOS][Wallet][SDK] Update path for API get all addresses (only in use)

--- sdk/ios/SoloSDK/SoloSDK/Classes/RestAPI/WalletAddressesAPI.swift
+++ sdk/ios/SoloSDK/SoloSDK/Classes/RestAPI/WalletAddressesAPI.swift
@@ -11,7 +11,7 @@ import Foundation
 public extension RESTService {
     // get addresses by wallet id
     public func getAddresses(_ id: String, completionHandler: completion = nil) {
-        let url = Configuration.BASE_URL + "/api/wallets/\(id)/addresses"
+        let url = Configuration.BASE_URL + "/api/wallets/\(id)/addresses?inUse=true"
         return self.execute(.get, url: url, completionHandler: completionHandler)
     }
 }
