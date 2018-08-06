02e4e07fc31155e5818ebf3965f79fa07e13c512
Hoang Nguyen <hoang.nn@big-labs.com>
2018-08-06 18:00:15 +0700

    [RN][Signer] Merge with desktop: Async Storage Wrapper.

---
+++ SOLOSigner/src/dao/AsyncDAO.js
@@ -0,0 +1,9 @@
+import {AsyncStorage} from 'react-native';
+export default class AsyncStorageFactory {
+    setItem(key, value){
+
+    }
+    getItem(key){
+
+    }
+} 
---
+++ SOLOSigner/src/dao/AsyncDAO.web.js
@@ -0,0 +1,8 @@
+export default class AsyncStorageFactory {
+    setItem(key, value){
+
+    }
+    getItem(key){
+
+    }
+} 
