415a4e2c28c224355402a15fe495808034f21420
hoang.pvm <hoang.pvm@big-labs.com>
2018-08-24 15:32:41 +0700

    [Desktop][Signer] Add Websocket support

--- SOLOSigner/desktop/main.js
+++ SOLOSigner/desktop/main.js
@@ -138,3 +138,31 @@ function showErrorDialog() {
     dialog.showErrorBox('Error Message', 'System has problem while processing.');
   })
 }
+
+function add(x, y){
+  console.log("Add function in main");
+  return x + y;
+}
+
+//over ws
+var WebSocketServer = new require('ws');
+var JsonRPC = require('simple-jsonrpc-js');
+ 
+var webSocketServer = new WebSocketServer.Server({
+    host: '0.0.0.0',
+    port: 8090
+}).on('connection', function(ws) {
+    var jrpc = new JsonRPC();
+    ws.jrpc = jrpc;
+ 
+    ws.jrpc.toStream = function(message){
+        ws.send(message);
+    };
+ 
+    ws.on('message', function(message) {
+        jrpc.messageHandler(message);
+    });
+ 
+    jrpc.on('add', ['x', 'y'], add);
+    
+});
