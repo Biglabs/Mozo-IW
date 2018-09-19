dcf086f9ea27da9bec54dcfc03cb86802daa2215
Hoang Nguyen <hoang.nn@big-labs.com>
2018-09-19 09:22:18 +0700

    [iOS][SDK][Transaction] Implement border layer for buttons.

--- sdk/ios/MozoSDK/MozoSDK/Classes/Modules/New Group/User Interface/View/TransferViewController.swift
+++ sdk/ios/MozoSDK/MozoSDK/Classes/Modules/New Group/User Interface/View/TransferViewController.swift
@@ -10,4 +10,23 @@ import UIKit
 
 class TransferViewController: MozoBasicViewController {
     
+    @IBOutlet weak var lbBalance: UILabel!
+    @IBOutlet weak var lbExchange: UILabel!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var btnAddressBook: UIButton!
+    @IBOutlet weak var btnScan: UIButton!
+    @IBOutlet weak var txtAmount: UITextField!
+    @IBOutlet weak var lbSpendable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBtnLayer()
+    }
+    
+    func setBtnLayer() {
+        btnAddressBook.layer.borderWidth = 1
+        btnAddressBook.layer.borderColor = ThemeManager.shared.main.cgColor
+        btnScan.layer.borderWidth = 1
+        btnScan.layer.borderColor = ThemeManager.shared.main.cgColor
+    }
 }
