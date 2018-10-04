bbb156980071ec7200f8a8fbdcff9a1e4552d3f0
Hoang Nguyen <hoang.nn@big-labs.com>
2018-10-04 11:33:43 +0700

    [iOS][Shopper] Update demo app with new method to init mozo sdk.

--- Shopper/Shopper/Classes/AppDelegate.swift
+++ Shopper/Shopper/Classes/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        MozoSDK.configure(key: "")
-        
+        MozoSDK.configure()
         return true
     }
     
