4a5be963b1b15ae179bbbbfea82850ccc6ac8bb0
HoangNguyen <hoang.nn@big-labs.com>
2018-10-07 22:45:56 +0700

    [iOS][SDK][UI][TransactionCompletion] Add image to show loading.

---
+++ sdk/ios/MozoSDK/MozoSDK/Classes/Modules/TxCompletion/Business Logic/TransactionStatusService.swift
@@ -0,0 +1,28 @@
+//
+//  TransactionStatusService.swift
+//  MozoSDK
+//
+//  Created by HoangNguyen on 10/7/18.
+//
+
+import Foundation
+
+class TransactionStatusService: NSObject {
+    let apiManager: ApiManager
+    
+    init(apiManager : ApiManager) {
+        self.apiManager = apiManager
+    }
+    
+    func startService(hash: String) {
+        
+    }
+    
+    func stopService() {
+        
+    }
+    
+    func notify() {
+        
+    }
+}
