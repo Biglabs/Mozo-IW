067eeb7ecd8ce6b19f85aba97ae15496b2012580
Hoang Nguyen <hoang.nn@big-labs.com>
2018-10-08 14:17:44 +0700

    [iOS][SDK][TxCompletion] Implement: Apply new design for UI.

--- sdk/ios/MozoSDK/MozoSDK/Classes/Modules/TxCompletion/User Interface/View/TxCompletionViewController.swift
+++ sdk/ios/MozoSDK/MozoSDK/Classes/Modules/TxCompletion/User Interface/View/TxCompletionViewController.swift
@@ -12,25 +12,72 @@ class TxCompletionViewController: MozoBasicViewController {
     var eventHandler : TxCompletionModuleInterface?
     var detailItem: TxDetailDisplayItem!
     
+    @IBOutlet weak var txHashView: UIView!
+    @IBOutlet weak var txHashLabel: UILabel!
+    
+    @IBOutlet weak var txCompletionView: UIView!
+    @IBOutlet weak var txCpInfoLabel: UILabel!
+    @IBOutlet weak var txCpAddressLabel: UILabel!
+    
+    @IBOutlet weak var txStatusView: UIView!
+    @IBOutlet weak var txStatusImg: UIImageView!
+    @IBOutlet weak var txStatusLabel: UILabel!
+    
     @IBOutlet weak var lbAmount: UILabel!
     @IBOutlet weak var lbAddress: UILabel!
     @IBOutlet weak var btnSave: UIButton!
     @IBOutlet weak var btnDetail: UIButton!
+    @IBOutlet weak var btnReplaceDetail: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setBtnLayer()
+        startSpinnerAnimation()
         updateView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.title = ""
+        checkAddressBook()
+    }
+    
+    func checkAddressBook() {
+        print("Check address book")
+        // Check address book
+        // Verify address is existing in address book list or not
+        let list = SessionStoreManager.addressBookList
+        let contain = AddressBookDTO.arrayContainsItem(detailItem.addressTo, array: list)
+        if contain {
+            btnSave.isHidden = true
+            btnDetail.isHidden = true
+            btnReplaceDetail.isHidden = false
+        }
     }
     
     func updateView() {
-        lbAddress.text = detailItem.addressTo
-        lbAmount.text = "{\(detailItem.amount)}"
+        txHashLabel.text = detailItem.hash
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+    }
+    
+    func startSpinnerAnimation() {
+        rotateView()
+    }
+    
+    private func rotateView(duration: Double = 1.0) {
+        UIView.animate(withDuration: duration, delay: 0.0, options: .curveLinear, animations: {
+            self.txStatusImg.transform = self.txStatusImg.transform.rotated(by: CGFloat.pi)
+        }) { finished in
+            self.rotateView(duration: duration)
+        }
+    }
+    
+    func stopSpinnerAnimation() {
+        txStatusImg.layer.removeAllAnimations()
     }
     
     func setBtnLayer() {
@@ -38,6 +85,8 @@ class TxCompletionViewController: MozoBasicViewController {
         btnSave.layer.borderColor = ThemeManager.shared.main.cgColor
         btnDetail.layer.borderWidth = 1
         btnDetail.layer.borderColor = ThemeManager.shared.main.cgColor
+        btnReplaceDetail.layer.borderWidth = 1
+        btnReplaceDetail.layer.borderColor = ThemeManager.shared.main.cgColor
     }
     
     @IBAction func btnSaveTapped(_ sender: Any) {
